c40f92a863c6117e0e18f0df2c22750e22eb5723
Razia Pervez <160026294+RaziaPervez@users.noreply.github.com>
2024-03-05 11:58:27 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+//giving values to variable 
+var biodata = {
+    name: "Razia Pervez",
+    age: 18,
+    isMarried: false
+};
+// console whole biodata
+console.log(biodata);
---
+++ main.ts
@@ -0,0 +1,16 @@
+//using variable 
+interface biodata{
+    name:string;
+    age:number;
+    isMarried:boolean;
+}
+
+//giving values to variable 
+let biodata={
+    name:"Razia Pervez",
+    age:18,
+    isMarried:false
+}
+
+// console whole biodata
+console.log(biodata)
